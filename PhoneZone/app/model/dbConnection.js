826a544fb6a495731b0d9de09cccb7eb18de52ae
Lava-shr <31430042+Lava-shr@users.noreply.github.com>
2021-07-14 11:45:50 +1000

    Update dbConnection.js

--- PhoneZone/app/model/dbConnection.js
+++ PhoneZone/app/model/dbConnection.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 require('../model/user');
 require('../model/listing');
 
-// connect to mongodb username: admin; pass:comp5347
-const dbURI = 'mongodb+srv://admin:comp5347@cluster0.st1oo.mongodb.net/phoneZone?retryWrites=true&w=majority';
+// connect to mongodb need right username and passowrd 
+const dbURI = 'mongodb+srv://Username:password@cluster0.st1oo.mongodb.net/phoneZone?retryWrites=true&w=majority';
 const dbConnection = mongoose.connect(dbURI, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false } )
                     .then((result) => console.log("DB connection established"))
                     .catch((err) => console.log(err));
